86ff6f041b9867ab3a6b01cc24be928b311ad4b1
Scott Mebberson <scott@scottmebberson.com>
2011-07-25 09:35:31 +0930

    Added some basic styling, interface functionality and setup sass

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(function(){
 
-});
+	$('#getStarted').click(function () {
+		
+		$('#firstTime').fadeOut('fast', function () {
+			
+			$('#editor').fadeIn('slow');
+			
+		});
+		
+	});
+	
+	$('#save').click(function () {
+		
+		alert('Woah there bad-boy, you can\'t save anything yet.');
+		
+	});
+
+});
