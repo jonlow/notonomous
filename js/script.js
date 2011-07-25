2ffe5bc097c61cb08a0a51d64132f603cdd60ef3
Scott Mebberson <scott@scottmebberson.com>
2011-07-25 22:55:43 +0930

    You can now create multiple notes
    Integrated a localStorage backbone extension (the default as advertised
    by backbone itself) The application switches between a first time view,
    and the 'your notes' list depending on if you have any notes in local
    storage Added the ability to create multiple notes Added a list view,
    showing all notes in local storage Added some default styling (needs
    work)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,163 @@
 $(function(){
 
-	$('#getStarted').click(function () {
+	$('.createNote').click(function () {
+		createNote();
+	});
+	
+	createNote = function () {
 		
-		$('#firstTime').fadeOut('fast', function () {
-			
+		// create a new Note
+		window.currentNote = new Note();
+		
+		$('#firstTime,#notes').fadeOut('fast', function () {
 			$('#editor').fadeIn('slow');
-			
 		});
+		
+	}
+	
+	/*
+		Backbone
+	*/
+	
+	// the note model
+	window.Note = Backbone.Model.extend({
+		
+		localStorage: new Store("notes"),
+		
+		initialize: function () {
+			this.set({"created": new Date()});			
+		},
+		
+		defaults: {
+			title: 'title...',
+			content: 'content...',
+			created: null,
+			lastSaved: null,
+			bMarkup: false
+		}
 		
 	});
 	
-	$('#save').click(function () {
+	// a collection of notes
+	window.NotesList = Backbone.Collection.extend({
 		
-		alert('Woah there bad-boy, you can\'t save anything yet.');
+		model: Note,
+		
+		localStorage: new Store("notes")
 		
 	});
-
+	window.Notes = new NotesList();
+	
+	Notes.bind("add", function (note) {
+		log('new note: ' + note.get('title'));
+	});
+	
+	Notes.bind("reset", function (col) {
+		log('Notes.reset: ' + col.length);
+	});
+	
+	// a list of notes
+	window.NotesView = Backbone.View.extend({
+		
+		tagName: "li",
+		className: "note-list",
+		
+		template: _.template($('#note-list-template').html()),
+		
+		initialize: function () {
+			
+			_.bindAll(this, 'render');
+			
+			this.model.bind('change', this.render);
+			this.model.view = this;
+			
+		},
+		
+		render: function () {
+			
+			$(this.el).html(this.template(this.model.toJSON()));
+			this.setContent();
+			return this;
+			
+		},
+		
+		setContent: function () {
+			
+			var title = this.model.get('title');
+			
+			this.$('h1').text(title);
+			
+		}
+		
+	});
+	
+	// the app itself
+	window.NotonomousView = Backbone.View.extend({
+		
+		el: $("#notonomous"),
+		
+		events: {
+			"click #save": "saveNote"
+		},
+		
+		initialize: function () {
+			
+			log('NotonomousView.initialize');
+			
+			_.bindAll(this, 'notesAdd', 'notesReset', 'render');
+			
+			Notes.bind('add', this.notesAdd);
+			Notes.bind('reset', this.notesReset);
+			Notes.bind('reset', this.notesBootstrap);
+			Notes.bind('all', this.render);
+			
+			Notes.fetch();
+			
+		},
+		
+		notesAdd: function (note) {
+			log('notesAdd');
+			var view = new NotesView({model: note});
+			this.$('#notes-list').append(view.render().el);
+		},
+		
+		notesReset: function () {
+			log('notesReset');
+			Notes.each(this.notesAdd);
+		},
+		
+		notesBootstrap: function () {
+			$('#' + (Notes.length === 0 ? 'firstTime' : 'notes')).fadeIn('slow');
+		},
+		
+		render: function () {
+			log('render');
+		},
+		
+		getNoteAttributes: function () {
+			
+			return {
+				title: $('#title').val(),
+				content: $('#content').val()
+			};
+			
+		},
+		
+		saveNote: function (e) {
+			
+			Notes.create(this.getNoteAttributes());
+			
+			$('#title').val('');
+			$('#content').val('');
+			
+			$('#editor').fadeOut('fast', function () {
+				$('#notes').fadeIn('fast');
+			});
+			
+		}
+		
+	});
+	
+	window.App = new NotonomousView;
+	
 });
