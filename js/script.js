e1c11fe79b9077be39cafb11f8ed19f235a52999
Scott Mebberson <scott@scottmebberson.com>
2011-07-27 16:06:53 +0930

    Added the ability to cancel an edit, and to print a note

--- js/script.js
+++ js/script.js
@@ -200,13 +200,18 @@ $(function(){
 		routes: {
 			"!/local/create": "createNote",
 			"!/local/:note": "editNote",
-			"!/list": "list"
+			"!/list": "list",
+			"!/print": "print"
 		},
 		
 		createNote: function () {
 			
 			// ok, let's create a new note!
 			window.currentNote = new Note();
+			
+			// clear the editing interface ui
+			$('#title').val('');
+			$('#content').val('');
 
 			$('#firstTime,#notes').fadeOut('fast', function () {
 				$('#editor').fadeIn('slow');
@@ -220,11 +225,28 @@ $(function(){
 		
 		list: function () {
 			
-			$('#preview').fadeOut('fast', function () {
-				$('#notes').fadeIn('fast');
-			});
+			// this could be much better, but is fine for now
+			if ($('#preview').is(":visible")) {
+				
+				$('#preview').fadeOut('fast', function () {
+					$('#notes').fadeIn('fast');
+				});
+				
+			} else if ($('#editor').is(":visible")) {
+				
+				$('#editor').fadeOut('fast', function () {
+					$('#notes').fadeIn('fast');
+				});
+				
+			}
 			
 			AppController.navigate("");
+			
+		},
+		
+		print: function () {
+			
+			$('#preview iframe')[0].contentWindow.print();
 			
 		}
 		
